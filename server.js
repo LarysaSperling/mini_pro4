0b343cc6debe29e4c652097e0aa8b708051b83a8
Larysa <lorasperling@gmail.com>
2026-06-16 10:03:42 +0200

    Configure Express server and database connection

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Server running on port ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
